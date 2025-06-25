5644050b85245827b6d99aac706353bcb54e4e80
Grant Hughes <granthughes636@gmail.com>
2025-06-25 16:42:02 -0500

    added assignment and poroject

---
+++ Assignments/Recipe/RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Grant David Hughes on 6/25/25.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
